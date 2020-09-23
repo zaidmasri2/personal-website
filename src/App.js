54f66e9b55e803e487a0383faaa4ebf923cc897b
Hawyar <hawyarxoshnaw@gmail.com>
2020-09-23 13:12:30 -0400

    add: shared layout, fix smooth scroll

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Projects from "./components/Projects";
-import About from "./components/About";
-import Landing from "./components/Landing";
-import Contact from "./components/Contact";
-import "./App.css";
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Projects from './components/Projects';
+import About from './components/About';
+import Layout from './components/Layout';
+import Landing from './components/Landing';
+import Contact from './components/Contact';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Landing />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
+    <div className='App'>
+      <Layout>
+        <Header />
+        <Landing />
+        <Projects />
+        <Contact />
+        <Footer />
+      </Layout>
     </div>
   );
 }
